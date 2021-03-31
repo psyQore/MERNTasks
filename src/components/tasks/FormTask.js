5341d35d5d068fece13e134ceec64467bdbdd3b5
Juan Galarce <j6alarce@gmail.com>
2021-03-31 11:55:45 -0300

    Ocultando el formTasks si no hay projecto seleccionado

--- src/components/tasks/FormTask.js
+++ src/components/tasks/FormTask.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import projectContext from "../../context/projects/projectContext";
 
 const FormTask = () => {
+
+    // Extraer si un proyecto esta activo
+    const projectsContext = useContext(projectContext);
+    const { project } = projectsContext;
+
+      // Si no hay project seleccionado
+  if (!project) return null;
+
+  // Array destructuring para extraer el project actual
+  const [projectActual] = project;
+  
   return (
     <div className="formulario">
       <form>
