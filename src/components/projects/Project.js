bac55f26255ef38c3c3c2b7950ec1bacc7ff1efc
Juan Galarce <j6alarce@gmail.com>
2021-04-01 17:07:37 -0300

    Creando una funcion que obtenta las tasks del project

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,17 +1,27 @@
 import React, { useContext } from "react";
-import projectContext from "../../context/projects/projectContext";
+import ProjectContext from "../../context/projects/projectContext";
+import TaskContext from "../../context/tasks/taskContext";
 
 const Project = ({ project }) => {
   // Obtener el state del contextProject
-  const projectsContext = useContext(projectContext);
+  const projectsContext = useContext(ProjectContext);
   const { projectActual } = projectsContext;
+
+  const taskContext = useContext(TaskContext);
+  const { getTasks } = taskContext;
+
+  // Funcion para agregar el project actual
+  const selectProject = (id) => {
+    projectActual(id); // Fijar un project actual
+    getTasks(id); // Filtrar las tareas cuando se de click
+  };
 
   return (
     <li>
       <button
         type="button"
         className="btn btn-blank"
-        onClick={ () => projectActual(project.id)}
+        onClick={() => selectProject(project.id)}
       >
         {project.name}
       </button>
--- src/context/tasks/taskState.js
+++ src/context/tasks/taskState.js
@@ -2,6 +2,8 @@ import React, { useReducer } from "react";
 import TaskContext from "./taskContext";
 import TaskReducer from "./taskReducer";
 
+import { TASKS_PROJECT } from '../../types';
+
 const TaskState = (props) => {
   const initialState = {
     tasks: [
@@ -21,10 +23,21 @@ const TaskState = (props) => {
   // Crear el dispatch y state
   const [state, dispatch] = useReducer(TaskReducer, initialState);
 
+  // Crear las funciones
+
+  // Obtener las tasks de un project
+  const getTasks = projectId => {
+      dispatch({
+          type: TASKS_PROJECT,
+          payload: projectId
+      })
+  }
+
   return (
     <TaskContext.Provider
       value={{
         tasks: state.tasks,
+        getTasks,
       }}
     >
       {props.children}
